bf86c67deb6e3e56b2d97a4d0839d63e32dfa0ae
atsushi.yasutake <atsu-yasutake@wearcoord.com>
2023-12-27 08:55:57 +0900

    update test

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // return view('welcome');
-    return 'Hello World, Hello Laravel';
+    return view('welcome');
+    // return 'Hello World, Hello Laravel';
 });
